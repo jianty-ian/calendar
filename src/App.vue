238d9849a6923e14ceddb2291b858db99eccfab8
Jianty <B10741@mail.chimei.org.tw>
2022-10-07 16:28:07 +0800

    first ver

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div class="container">
+        <CalenderCard></CalenderCard>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalenderCard from './components/CalenderCard.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        CalenderCard
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
+.container{
+    padding: 0px 400px;
+}
+@media screen and (max-width:1904px) { 
+    .container{
+        padding: 0px 200px;
+    }
+}
+@media screen and (max-width:1264px) { 
+    .container{
+        padding: 0px 100px;
+    }
+}
+@media screen and (max-width:960px) { 
+    .container{
+        padding: 0px 50px;
+    }
+}
+
 </style>
---
+++ src/components/CalenderCard.vue
@@ -0,0 +1,209 @@
+<template>
+<div id="app" v-cloak>
+      <h1>{{calendar.year}} 年 {{calendar.month+1}} 月</h1>
+      <div>
+          <button @click="adjustYear(-1)">上一年</button>
+          <button @click="adjustMonth(-1)">上個月</button>
+          <button @click="setToday">今天</button>
+          <button @click="adjustMonth(1)">下個月</button>
+          <button @click="adjustYear(1)">下一年</button>
+      </div>
+      <div class="calendar">
+        <div class="weekDay">
+            <div>日</div>
+            <div>一</div>
+            <div>二</div>
+            <div>三</div>
+            <div>四</div>
+            <div>五</div>
+            <div>六</div>
+        </div>
+        <div class="week" v-for="i in 6" :key="i">
+          <div class="day" v-for="j in 7" :data-date="calendarMonth[(i-1)*7+j-1].date"
+            :class="{
+                a_class: calendarMonth[(i-1)*7+j-1].class_code == 0 && calendarMonth[(i-1)*7+j-1].month == calendar.month,
+                b_class: calendarMonth[(i-1)*7+j-1].class_code == 1 && calendarMonth[(i-1)*7+j-1].month == calendar.month,
+                c_class: calendarMonth[(i-1)*7+j-1].class_code == 2 && calendarMonth[(i-1)*7+j-1].month == calendar.month,
+                today:calendarMonth[(i-1)*7+j-1].year === today.year && calendarMonth[(i-1)*7+j-1].month === today.month && calendarMonth[(i-1)*7+j-1].date === today.date,
+                other:calendarMonth[(i-1)*7+j-1].month !== calendar.month,
+            }"
+            style="display:flex;justify-content:center;align-items: center;"
+            :key="j"
+          >
+            <span style="transform:translateY(15px)">
+                {{calendarMonth[(i-1)*7+j-1].class_name}}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CalenderCard',
+    props: {
+    },
+    data() {
+        return {
+            today:{
+                year:0,
+                month:0,
+                date:0,
+                day:0
+            },
+            calendar:{
+                year:0,
+                month:0,
+                date:0,
+                day:0
+            },
+            baseday:'1970-01-01',
+            class:[
+                "丙",
+                "甲",
+                "乙",
+            ]
+        };
+    },
+    mounted(){
+        this.setToday()
+        document.title = '南監三班班表'
+    },
+    methods:{
+        setToday(){
+            const date = new Date()
+            this.today.year = this.calendar.year = date.getFullYear()
+            this.today.month = this.calendar.month = date.getMonth() // 0~11
+            this.today.date = this.calendar.date = date.getDate()
+            this.today.day = this.calendar.day = date.getDay()
+        },
+        adjustYear(fix){
+            this.calendar.year += fix
+        },
+        adjustMonth(fix){
+            // this.calendar.month += fix 範圍
+            let month = this.calendar.month + fix
+            if(month > 11){
+            this.adjustYear(1)
+            this.calendar.month = 0
+            }else if(month < 0){
+            this.adjustYear(-1)
+            this.calendar.month = 11
+            }else{
+            this.calendar.month = month
+            }
+
+        },
+        DateDiff(Date1, Date2) { // date object 日期格式
+            let milliseconds_Time = Date2.getTime() - Date1.getTime();
+            return Math.floor(milliseconds_Time / (1000 * 3600 * 24));
+        }
+    },
+    computed:{
+        // monthFirstDay(){
+        //   const date = new Date(this.calendar.year,this.calendar.month,1)
+        //   return {
+        //     year:date.getFullYear(),// this.calendar.year
+        //     month:date.getMonth(),// this.calendar.month
+        //     date:date.getDate(),// 1
+        //     day:date.getDay(),
+        //   }
+        // },
+        calendarFirstDay(){
+            const mDate = new Date(this.calendar.year,this.calendar.month,1)
+            const date = new Date(this.calendar.year,this.calendar.month,1 - mDate.getDay())
+            return {
+                year:date.getFullYear(),
+                month:date.getMonth(),
+                date:date.getDate(),
+                day:date.getDay(),
+            }
+        },
+        calendarMonth(){
+            const data = []
+            let date
+            for(let i=0;i<42;i++){
+                date = new Date(this.calendarFirstDay.year,this.calendarFirstDay.month,this.calendarFirstDay.date + i)
+                let class_code = this.DateDiff(new Date(this.baseday),date) % 3
+                let class_name = this.class[class_code];
+                data.push({
+                    year:date.getFullYear(),
+                    month:date.getMonth(),
+                    date:date.getDate(),
+                    day:date.getDay(),
+                    class_code,
+                    class_name
+                })
+            }
+            return data
+        }
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+      
+    [v-cloak]{
+        display: none;
+    }
+
+    html{
+        text-align: center;
+    }
+
+    .calendar{
+        margin:30px 0;
+    }
+
+    .weekDay,.week{
+        display: flex;
+        border-bottom: 1px solid #ddd;
+    }
+    .weekDay>div{
+        flex: 1 1 0%;
+        line-height: 30px;
+    }
+    .week{
+        border-right: 1px solid #ddd;
+    }
+    .week>div{
+        position: relative;
+        flex: 1 1 0%;
+        height: 90px;
+        line-height: 30px;
+        border-left: 1px solid #ddd;
+    }
+    .week>div:before{
+        position: absolute;
+        content:attr(data-date);
+        top: 0;
+        right: 0;
+        display: block;
+        width: 30px;
+        border-left: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .today:before{
+        color: #fff;
+        background-color: #000;
+    }
+
+    .other{
+        color: #bbb;
+        background-color: #ddd;
+    }
+    .a_class{
+        background-color: rgba(255, 107, 107, 0.7);
+    }
+    .b_class{
+        background-color: rgba(255, 223, 78, 0.7);
+    }
+    .c_class{
+        background-color: rgba(52, 154, 223, 0.7);
+    }
+</style>
